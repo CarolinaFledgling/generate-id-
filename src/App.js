3ed77f4e0a18331123d20964b2372e7f7b794fcf
Karolina Kulinska <karolina.kulinska89@gmail.com>
2021-08-01 00:11:32 +0200

    addec functionality for creating uuid

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { v4 as uuidv4 } from "uuid";
+import { useState } from "react";
 
 function App() {
+  const [uuid, setUuid] = useState(uuidv4());
+
+  const generateId = () => {
+    setUuid(uuidv4());
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{uuid}</p>
+      <button onClick={generateId}>Generator ID</button>
     </div>
   );
 }
